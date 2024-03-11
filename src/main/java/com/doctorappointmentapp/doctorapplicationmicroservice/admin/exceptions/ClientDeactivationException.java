5ba47b6a959aad8eeeda4ac104c39c0141b8e30b
S <HS21@ford.com>
2024-03-11 23:48:07 +0530

    1

--- src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/admin/exceptions/ClientDeactivationException.java
+++
@@ -1,7 +0,0 @@
-package com.doctorappointmentapp.doctorapplicationmicroservice.admin.exceptions;
-
-public class ClientDeactivationException extends Exception{
-    public ClientDeactivationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/admin/exceptions/DoctorDeactivationException.java
+++
@@ -1,7 +0,0 @@
-package com.doctorappointmentapp.doctorapplicationmicroservice.admin.exceptions;
-
-public class DoctorDeactivationException extends Exception{
-    public DoctorDeactivationException(String message) {
-        super(message);
-    }
-}
