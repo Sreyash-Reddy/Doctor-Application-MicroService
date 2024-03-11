f52161bcc165c29d71c60430c39bd9eeccd48c65
S <HS21@ford.com>
2024-03-12 00:06:47 +0530

    Admin2

--- src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/appointment/Appointment.java
+++ src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/appointment/Appointment.java
@@ -1,4 +1,35 @@
 
+package com.doctorappointmentapp.doctorapplicationmicroservice.appointment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Appointment {
+
+    @GeneratedValue
+    @Id
+    private Integer id;
+    private String appointmentDescription;
+
+    private Boolean paymentStatus;
+    private Boolean doctorConfirmationStatus;
+
+    private LocalDate appointmentDate;
+    private Integer appointmentSlot;
+
+    private Integer clientID;
+    private Integer doctorID;
+}
 
 //Client
 
